1498139e7bbe64554613d885128e2abd3f3df135
Kenneth Chacon <ventasvg2022@gmail.com>
2026-02-09 16:49:58 -0600

    Primera versión Eventos VG

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+
+    return render_template('index.html')
+
+@app.route('/servicios')
+
+def servicios():
+
+    return render_template('servicios.html')
+
+@app.route('/contacto')
+
+def contacto():
+
+    return render_template('contacto.html')
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
+ 
+ 
+
+
+ 
---
+++ static/script.js
@@ -0,0 +1,26 @@
+function openWhatsApp() {
+
+  window.open("https://wa.me/506XXXXXXXX", "_blank");
+
+}
+
+function openFacebook() {
+
+  window.open("https://facebook.com/eventosespecialesvg", "_blank");
+
+}
+
+function openInstagram() {
+
+  window.open("https://instagram.com/eventosespecialesvg", "_blank");
+
+}
+ 
+ 
+ 
+
+
+
+ 
+
+ 
